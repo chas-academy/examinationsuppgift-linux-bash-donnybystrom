aae14c54c19002ed6f8f8abb62bc94344f90d43b
Donny Byström <donnybystrom@gmail.com>
2026-04-23 10:16:19 +0000

    Second pass - create user folders and write to welcome.txt

--- create_users.sh
+++ create_users.sh
@@ -21,3 +21,32 @@ for username in "$@"; do
         }
     fi
 done
+
+# Sätt upp kataloger och welcome.txt efter att alla användare finns.
+for username in "$@"; do
+    home_dir="/home/$username"
+    groupname="$(id -gn "$username")"
+
+    # Hämta alla användarnamn utom aktuell användare.
+    existing_users="$(cut -d: -f1 /etc/passwd | grep -vx "$username")"
+
+    # Skapa kataloger och sätt rättigheter (700 - Endast användaren får full CRUD till katalogerna).
+    mkdir -p "$home_dir/Documents" "$home_dir/Downloads" "$home_dir/Work"
+    chmod 700 "$home_dir/Documents" "$home_dir/Downloads" "$home_dir/Work"
+
+    # Skapa welcome.txt med en hälsning och listan på andra användare.
+    {
+        echo "Välkommen $username"
+        echo "$existing_users"
+    } > "$home_dir/welcome.txt"
+
+    # Sätt ägarskap på kataloger och welcome.txt
+    chown -R "$username:$groupname" \
+        "$home_dir/Documents" \
+        "$home_dir/Downloads" \
+        "$home_dir/Work" \
+        "$home_dir/welcome.txt"
+
+    # Sätt rättigheter på welcome.txt så att endast ägaren kan läsa + skriva till den.
+    chmod 600 "$home_dir/welcome.txt"
+done
